5cefca786691d52df588041a07e171179cf2634c
arturomf94 <arturomf94@gmail.com>
2018-09-18 13:55:52 -0500

    add jupyter notebook

---
+++ 3/topological_sort.py
@@ -0,0 +1,26 @@
+import networkx as nx
+
+# G = nx.DiGraph() 
+# G.add_nodes_from(list(range(1,4)))
+# G.add_edges_from([(1,2),(3,2)])
+
+# Return set of nodes with in-degree 0
+def in_degree_0(G):
+	S = []
+	nodes = [node for (node, val) in G.nodes().items()]
+	for n in nodes:
+		if G.in_degree(n) == 0:
+			S.append(n)
+	return S
+
+def topological_sort(G):
+	L = []
+	S = in_degree_0(G)
+	while S != []:
+		node = S[0]
+		L.append(node)
+		G.remove_node(node)
+		S = in_degree_0(G)
+	if G.number_of_edges() > 0:
+		return 'G contiene ciclos!'
+	return 'Orden: ' + ' '.join(str(e) for e in L)
